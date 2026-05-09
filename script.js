e6345bd3f47a3570af8c10b0521ee83e0c83b01a
Nilo Arthur <nilo.ribeiro@aluno.senai.br>
2026-05-09 12:12:16 -0300

    Refactor API error handling and update fetch URLs

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ async function pucharAPI1() {
     try {
         const response = await fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL`)
         const dados = await response.json()
-        if(!response.ok){
+        if (!response.ok) {
             alert("um erro no json")
+            return
         }
         const inputDolar = document.querySelector(`#inputDolar`).value = dados.USDBRL.high
 
@@ -14,13 +15,18 @@ async function pucharAPI1() {
     }
     pucharAPI2()
 }
-async function pucharAPI2(){
-    try{
-        const response = await fetch(`http://ipwho.is/`)
+async function pucharAPI2() {
+    try {
+
+        const response = await fetch(`https://ipwho.is/`)
         const dados = await response.json()
 
-        const inputpais = document.querySelector(`#inputpais`).value = dados.country
-    } catch (error){
+         if (!response.ok) {
+            alert(`Erro HTTP: ${response.status} - ${response.statusText}`)
+            return
+        }
+        const inputpais = document.querySelector(`#inputpais`).value = dados.ip
+    } catch (error) {
         console.error(error)
     }
 }
