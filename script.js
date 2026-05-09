a46b67a4d6dc18e202a51b909cde0baf3e917881
Nilo Arthur <nilo.ribeiro@aluno.senai.br>
2026-05-09 17:39:40 -0300

    Refactor error handling and async function structure

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function pucharAPI2() {
         const response = await fetch(`https://restcountries.com/v3.1/name/brazil`)
         const dados = await response.json()
 
-         if (!response.ok) {
+        if (!response.ok) {
             alert(`Erro HTTP: ${response.status} - ${response.statusText}`)
             return
         }
@@ -30,31 +30,49 @@ async function pucharAPI2() {
         const inputcapital = document.querySelector(`#inputcapital`).value = dados[0].capital
         const inputpopulacao = document.querySelector(`#inputpopulacao`).value = dados[0].population
         // dados[0] para entrar no arrey 
-        
+
     } catch (error) {
         console.error(error)
     }
-    
+
 }
 const imgDog = document.querySelector(`#imgDog`)
-async function pucharAPI3(){
-    
-    try{
+async function pucharAPI3() {
+
+    try {
         const response = await fetch(`https://dog.ceo/api/breeds/image/random`)
         const dados = await response.json()
 
-        if(!response.ok){
+        if (!response.ok) {
             alert(`erro http: ${response.status} - ${response.statusText}`)
         }
-        
+
         imgDog.src = dados.message
         // alert(dados.message)
     }
-        catch (error){
-            console.error(error)
-        }
+    catch (error) {
+        console.error(error)
+    }
 }
-function apagarImg(){
+function apagarImg() {
     imgDog.src = ""
+}
+async function pucharAPI4() {
+    const inputKey = document.querySelector('#inputKey').value
+    const inputCidade = document.querySelector('#inputCidade').value
+
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCidade}&appid=${inputKey}&units=metric&lang=pt_br`)
+        const dados = await response.json()
+        if (!response.ok) {
+            alert(`Erro: ${response.status} - ${response.statusText}`)
+            return
+        }
+        const inputClima = document.querySelector('#inputClima').value = dados.weather[0].description
+        // weather[0] acessar o arrey 
+    }
+    catch (error) {
+        console.error(error)
+    }
 }
 pucharAPI3()
